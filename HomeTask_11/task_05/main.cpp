e74787415a1ac1c2061be466dd3d90d9dff3fd8a
pasha <pasha@employee.ru>
2021-07-20 20:12:57 +0300

    добавил анализ хода игры

--- HomeTask_11/task_05/main.cpp
+++ HomeTask_11/task_05/main.cpp
@@ -14,7 +14,7 @@ const string VANYA_OWON = "Vanya won";
 const string NOBODY     = "Nobody";
 
 string checkField(array<string, SIZE>);
-string getResult(string);
+string getResult(string, int, int, int);
 
 int main() {
   array<string, SIZE> field;
@@ -25,7 +25,16 @@ int main() {
   cin >> field[2];
   cout << endl;
 
-  cout << "result = " << getResult(checkField(field)) << endl << endl;
+  int countX = 0;    // количество X
+  int countO = 0;    // количество O
+  int countDot = 0;  // количество .
+  string str = field[0] + field[1] + field[2];
+  for (size_t i = 0; i < str.length(); i++) {
+    if (str[i] == 'X') { countX++; }
+    else if (str[i] == 'O') { countO++; }
+    else { countDot++; }
+  }
+  cout << getResult(checkField(field), countX, countO, countDot) << endl << endl;
 
   return 0;
 }
@@ -57,27 +66,41 @@ string checkField(array<string, SIZE> field) {
   return str;
 }
 
-string getResult(string str) {
-  int countX = 0; // число победивших комбинаций X
-  int countO = 0; // число победивших комбинаций O
+string getResult(string str, int countX, int countO, int countDot) {
+  int countWinX = 0; // число победивших комбинаций X
+  int countWinO = 0; // число победивших комбинаций O
+
   if (str == INCORRECT) {
-      return INCORRECT; // некорректный ввод игрового поля
+      return INCORRECT + "  некорректный ввод игрового поля ";
   }
   for (size_t pos = 0; pos <= str.length() - 3; pos += 3) {
     if (str.substr(pos, 3) == "XXX") {
-      countX++;
+      countWinX++;
     } else if (str.substr(pos, 3) == "OOO") {
-      countO++;
+      countWinO++;
     }
   }
-  if (countX > 1 || countO > 1) {
-    return INCORRECT; // некорректный ввод игрового поля
-  } else if (countX == 1 && countO == 0) {
+
+  if (countWinX > 1 || countWinO > 1 || (countWinX == 1 && countWinO == 1)) {
+    return INCORRECT + "  некорректный ввод игрового поля ";
+  } else if (countO > countX) {
+    str =  INCORRECT + "  ноликов в конце игры не может быть больше ";
+  } else if (countWinX == 0 && countWinO == 1 && (countO > countX)) {
+    str =  INCORRECT + "  нолики победили, но при этом их не может быть больше";
+  } else if (countWinX == 0 && countWinO == 0 && countDot != 0) {
+    str = NOBODY + "  потому что они не доиграли";
+  } else if (countWinX == 0 && countWinO == 0 && countDot == 0) {
+    str = NOBODY + "  потому что это ничья";
+  } else if (countWinX == 0 && countWinO == 1 && (countO < countX)) {
+    str =  INCORRECT + "  нолики победили, но при этом крестиков не может быть больше";
+  } else if (countWinX == 1 && countWinO == 0 && (countO == countX)) {
+    str =  INCORRECT + "  крестики победили, но при этом ноликов не может быть столькоже";
+  } else if (countWinX == 1 && countWinO == 0 && (countO < countX)) {
     str = PETYA_XWON;
-  } else if (countX == 0 && countO == 1) {
+  } else if (countWinX == 0 && countWinO == 1 && (countO == countX)) {
     str = VANYA_OWON;
   } else {
-    str = NOBODY;
+    str = "ERROR: Something went wrong ";
   }
 
   return str;
