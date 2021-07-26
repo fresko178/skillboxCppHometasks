229c7e6962b7f4c9b36e0b8bd1411e75fe686e6a
pasha <pasha@employee.ru>
2021-07-26 14:51:28 +0300

    переделал, сложность менее O(n^2)

--- HomeTask_12/task_02/main.cpp
+++ HomeTask_12/task_02/main.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
-#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
-using std::string;
 
 int main() {
   cout << "Задача 2. Ряд чисел" << endl << endl;
-  int numbers [] {1, 3, 2, 4, 5, 7, 10, 13, 9, 6, 11, 12, 8 ,13, 14};
+  int numbers [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 12, 13 ,14, 15};
   for (int i = 0; i < 15; i++) { // просто выводим для наглядности
     cout << "  " << numbers[i];
   }                  
-  cout << endl;
-  for (int i = 0; i < 15; i++) {
-    for (int j = 0; j < 15; j++) {
-      if (i != j && (numbers[i] == numbers[j])) {
-        cout << "повторяющееся число: " << numbers[i] << endl;
-        return 0; // валим, лишнюю работу не делаем
-      }
+  cout << endl << endl;
+  for (int i = 0; i < 14; i++) {
+    if (numbers[i] == numbers[i + 1]) {
+      cout << "повторяющееся число: " << numbers[i] << endl;
+      return 0; // лишнюю работу не делаем
     }
   }
 
