6e9a23cf49db4b068087ca08f5f1f779a4b33427
pasha <pasha@employee.ru>
2021-07-27 12:14:33 +0300

    сделал решение через сумму

--- HomeTask_12/task_02/main.cpp
+++ HomeTask_12/task_02/main.cpp
@@ -4,21 +4,17 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+constexpr int magic {1561};
+
 int main() {
   cout << "Задача 2. Ряд чисел" << endl << endl;
-  int numbers [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 12, 13 ,14, 15};
-  for (int i = 0; i < 15; i++) { // просто выводим для наглядности
-    cout << "  " << numbers[i];
-  }                  
-  cout << endl << endl;
-  for (int i = 0; i < 14; i++) {
-    if (numbers[i] == numbers[i + 1]) {
-      cout << "повторяющееся число: " << numbers[i] << endl;
-      return 0; // лишнюю работу не делаем
-    }
+  int numbers [] {114, 111, 106, 107, 118, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118};
+  int sum {0};
+  for (int i = 0; i < 15; i++) {
+    sum += *(numbers + i); // сумма всех элементов
+    cout << "  " << *(numbers + i);
   }
-
-  cout << endl;
+  cout << endl << endl << "повторяющееся число: " << sum - magic << endl << endl;
 
   return 0;
 }
