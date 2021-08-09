bd1c0950a436d8950e99d582780b3c0999ee32e9
pasha <pasha@employee.ru>
2021-08-09 17:01:12 +0300

    убрано static_cast из аргумента showSlice используемую в main

--- HomeTask_14/task_08/main.cpp
+++ HomeTask_14/task_08/main.cpp
@@ -19,16 +19,16 @@ int main() {
   cout << "Задача 8. Почти-Майнкрафт" << endl;
   Map map(LENGTH, vector<vector<bool>>(WIDTH, vector<bool>(HEIGHT, false))); // наш куб
   setMap(map); // устанавливаем в true ячейки столбцов, кроме ячеек превышающих значение высоты
-  int level = -1;
-  while (-1 == level) {
+  size_t level (SIZE_MAX);
+  while (SIZE_MAX == level) {
     cout << "Введите номер уровня среза (0-9): ";
     cin >> level;
     if (level < 0 || level > HEIGHT - 1) {
-      level = -1;
+      level = SIZE_MAX;
       cout << "[ERROR] Введён несуществующий уровень, повторите попытку" << endl;
     }
   }
-  showSlice(map, static_cast<size_t>(level));
+  showSlice(map, level);
 
   cout << endl << endl;
   return 0;
