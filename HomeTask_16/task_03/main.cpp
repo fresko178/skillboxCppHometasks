6306563c8418035bc15c69088ebc298e0502e359
pasha <pasha@employee.ru>
2021-08-30 13:58:58 +0300

    &string заменено на string_view, if-else-if на switch

--- HomeTask_16/task_03/main.cpp
+++ HomeTask_16/task_03/main.cpp
@@ -1,48 +1,59 @@
 #include <iostream>
 #include <string>
-#include <sstream>
+#include <string_view>
+#include <vector>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::string_view;
+using Vec = std::vector<string>;
 
-string InsertSpaces(const string &);
+Vec Pars(const string_view);
 
 int main() {
   cout << "Калькулятор" << endl << endl;
-  std::stringstream strBuffer;
   cout << "Введите выражение (<число> <+,-,*,/> <число>): ";
   string str;
-  cin >> str;
-  strBuffer << InsertSpaces(str); // теперь у нас 3 подстроки
-  strBuffer >> str; // извлекаем первую подстроку т.е. число
-  double num1 = std::stod(str);
-  string sign;
-  strBuffer >> sign; // извлекаем вторую подстроку, она всегда будет знак
-  strBuffer >> str; // извлекаем третью подстроку т.е. число
-  if (sign == "+") { cout << "result: " << num1 + std::stod(str); }
-  else if (sign == "-") { cout << "result: " << num1 - std::stod(str); }
-  else if (sign == "*") { cout << "result: " << num1 * std::stod(str); }
-  else if (sign == "/") { cout << "result: " << num1 / std::stod(str); }
+  cin >> str; // т.к. без пробелов можно и cin
+  string_view sv{str};
+  Vec vec = Pars(sv); // теперь у нас 3 подстроки
+  switch (vec[1][0]) {
+    case '+': cout << "result: " << std::stod(vec[0]) + std::stod(vec[2]);
+      break;
+    case '-': cout << "result: " << std::stod(vec[0]) - std::stod(vec[2]);
+      break;
+    case '*': cout << "result: " << std::stod(vec[0]) * std::stod(vec[2]);
+      break;
+    case '/': cout << "result: " << std::stod(vec[0]) / std::stod(vec[2]);
+      break;
+  }
+
   cout << endl << endl;
 
   return 0;
 }
 
-// разделяет строку на подстроки пробелами
-string InsertSpaces(const string &str) {
+// возвращает вектор строк, первое число, знак и третье число
+Vec Pars(const string_view str) {
   string newStr = "";
+  Vec vec;
   newStr += str[0]; // O(1)
   bool find = false; // признак того, что знак (не)найден
   for (size_t i = 1; i < str.length(); i++) { // O(n)
     if (!find && ('-' == str[i] || '+' == str[i] || '*' == str[i] || '/' == str[i])) { // шама ждёт знак
-      newStr = newStr + " " + str[i] + " ";
+      vec.push_back(newStr); // запоминаем первое число
+      newStr = str[i];
+      vec.push_back(newStr); // теперь запоминаем знак
       find = true;
+      newStr = "";
     } else {
       newStr += str[i];
     }
   }
-  return newStr;
+  vec.push_back(newStr); // третье число
+
+  return vec;
 }
 
