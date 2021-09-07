6506b8ad42cd69c0d14d9522077e27a279cb203c
pasha <pasha@employee.ru>
2021-09-07 15:55:13 +0300

    Переписана функция recursion

--- HomeTask_18/task_02/main.cpp
+++ HomeTask_18/task_02/main.cpp
@@ -3,39 +3,23 @@
 using std::cout;
 using std::endl;
 
-int recursion(int *, int, int, int, int &);
+int recursion(int purpose);
 
 int main() {
   cout << "Задача 2." << endl << endl;
   cout << "Введите номер ступеньки: ";
   int purpose;
   std::cin >> purpose;
-  int arr[purpose];    // массив который хранит комбинации ступенек
-  int count {0}; // кол. способов
-  count = recursion(arr, purpose, 0, 1, count);
-  cout << "Кол. способов = " << count;
+  cout << "Кол. способов = " << recursion(purpose) << endl;
 
   cout << endl << endl;
   return 0;
 }
 
-// arr - массив который хранит тек. комбинации ступенек
-// purpose – номер ступеньки
-// pos - позиция в массиве
-// elem – элемент массива
-// count - кол. способов
-int recursion(int *arr, int purpose, int pos, int elem, int &count) {
-  for (auto i = elem ; i <= purpose - 1; i++) {
-    arr[pos] = i;
-    recursion(arr, purpose - i, pos + 1, i, count);
-  }
-  count++;
-  cout << count << "]  (";
-  for (int i = 0 ; i < pos; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << purpose << ") " << endl;
+int recursion(int purpose) {
+  if (purpose < 0) { return 0; }
+  if (purpose == 0) { return 1; }
 
-  return count;
+  return recursion(purpose - 3) + recursion(purpose - 2) + recursion(purpose - 1);
 }
 
