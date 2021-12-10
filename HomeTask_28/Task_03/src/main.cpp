8a82392f69422e936d7c051966e767a77b3b3b89
fresko178 <fresko178@fresko178.free>
2021-12-10 11:31:09 -0500

    delete mutex

--- HomeTask_28/Task_03/src/main.cpp
+++ HomeTask_28/Task_03/src/main.cpp
@@ -2,27 +2,28 @@
 #include <thread>
 #include <vector>
 #include <mutex>
+#include <time.h>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 std::mutex access_cooking;
-std::mutex access_order;
 std::vector <std::string> orderBox;
 
 void kitchen(std::string *, int);
-int courier();
+void courier();
 
 int main() {
   std::string dishes[5] {"pizza", "soup", "steak", "salad", "sushi"};
-
   std::thread th(courier);
 
   for (size_t i {0}; i < 10; i++) {
     std::this_thread::sleep_for(std::chrono::seconds(std::rand() % 6 + 5));
     int index = std::rand() % 5;
-    cout << "the order was received (" + dishes[index] + ")" << endl;
+    time_t t = time(nullptr); // фиксируем время получения заказа
+    std::tm *local = std::localtime(&t);
+    cout << "the order was received (" + dishes[index] + ")  " << local->tm_min << ":" << local->tm_sec << endl;
     std::thread cooking(kitchen, dishes, index);
     cooking.detach();
   }
@@ -34,25 +35,27 @@ int main() {
 
 void kitchen(std::string *dishes, int index) {
   access_cooking.lock();
-  cout << "the order is accepted (" + dishes[index] + ")" << endl;
+  time_t t = time(nullptr); // фиксируем время поступления заказа на кухню
+  std::tm *local = std::localtime(&t);
+  cout << "the order is accepted (" + dishes[index] + ")  " << local->tm_min << ":" << local->tm_sec << endl;
   std::this_thread::sleep_for(std::chrono::seconds(std::rand() % 11 + 5));
-  cout << "the order is ready (" + dishes[index] + ")" << endl;
-  orderBox.push_back(dishes[index]);
+  t = time(nullptr);  // фиксируем время приготовления заказа
+  local = std::localtime(&t);
+  cout << "the order is ready (" + dishes[index] + ")  " << local->tm_min << ":" << local->tm_sec << endl;
+  orderBox.push_back(dishes[index]); // помещаем готовый заказ на стойку
   access_cooking.unlock();
 }
 
-int courier() {
-  int countOrders {0};
+void courier() {
+  int countOrders {0}; // счётчик заказов
   while (countOrders < 10) {
     std::this_thread::sleep_for(std::chrono::seconds(30));
-    access_order.lock();
-    countOrders += orderBox.size();
-    orderBox.clear();
-    cout << "courier received orders" << endl;
-    cout << "countOrders = " <<  countOrders << endl;
-    access_order.unlock();
+    countOrders += orderBox.size(); // курьер
+    orderBox.clear();               // забирает заказы
+    time_t t = time(nullptr); // фиксируем время выдачи заказов
+    std::tm *local = std::localtime(&t);
+    cout << "courier received orders    " << local->tm_min << ":" << local->tm_sec
+         << "     countOrders = " <<  countOrders << endl;
   }
-
-  return countOrders;
 }
 
